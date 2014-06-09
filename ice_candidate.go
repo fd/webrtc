3b7882578ebdf42f1a79092470c4c10bb5743028
Simon Menke <simon.menke@gmail.com>
2014-06-09 14:55:41 +0200

    gofmt

--- ice_candidate.go
+++ ice_candidate.go
@@ -7,67 +7,67 @@ package webrtc
 import "C"
 
 import (
-  "errors"
-  "os"
-  "unsafe"
+	"errors"
+	"os"
+	"unsafe"
 )
 
 type IceCandidate struct {
-  SdpMid        string `json:"sdp_mid"`
-  SdpMlineIndex int    `json:"sdp_mline_index"`
-  Candidate     string `json:"candidate"`
+	SdpMid        string `json:"sdp_mid"`
+	SdpMlineIndex int    `json:"sdp_mline_index"`
+	Candidate     string `json:"candidate"`
 }
 
 func (s *PeerConnection) AddIceCandidate(candidate *IceCandidate) error {
-  if s == nil || candidate == nil {
-    return os.ErrInvalid
-  }
+	if s == nil || candidate == nil {
+		return os.ErrInvalid
+	}
 
-  cerr := C.c_PeerConnection_AddIceCandidate(unsafe.Pointer(s.ptr), unsafe.Pointer(candidate))
-  if cerr != nil {
-    defer C.free(unsafe.Pointer(cerr))
-    return errors.New(C.GoString(cerr))
-  }
+	cerr := C.c_PeerConnection_AddIceCandidate(unsafe.Pointer(s.ptr), unsafe.Pointer(candidate))
+	if cerr != nil {
+		defer C.free(unsafe.Pointer(cerr))
+		return errors.New(C.GoString(cerr))
+	}
 
-  return nil
+	return nil
 }
 
 //export go_IceCandidate_ToGo
 func go_IceCandidate_ToGo(sdp_mid *C.char, sdp_mline_index C.int, sdp *C.char) unsafe.Pointer {
-  candidate := &IceCandidate{
-    SdpMid:        C.GoString(sdp_mid),
-    SdpMlineIndex: int(sdp_mline_index),
-    Candidate:     C.GoString(sdp),
-  }
-  return unsafe.Pointer(candidate)
+	candidate := &IceCandidate{
+		SdpMid:        C.GoString(sdp_mid),
+		SdpMlineIndex: int(sdp_mline_index),
+		Candidate:     C.GoString(sdp),
+	}
+	return unsafe.Pointer(candidate)
 }
 
 //export go_IceCandidate_GetSdpMid
 func go_IceCandidate_GetSdpMid(ptr unsafe.Pointer) *C.char {
-  if ptr == nil {
-    return C.CString("")
-  }
+	if ptr == nil {
+		return C.CString("")
+	}
 
-  candidate := (*IceCandidate)(ptr)
-  return C.CString(candidate.SdpMid)
+	candidate := (*IceCandidate)(ptr)
+	return C.CString(candidate.SdpMid)
 }
 
 //export go_IceCandidate_GetSdpMlineIndex
 func go_IceCandidate_GetSdpMlineIndex(ptr unsafe.Pointer) C.int {
-  if ptr == nil {
-    return -1
-  }
+	if ptr == nil {
+		return -1
+	}
 
-  candidate := (*IceCandidate)(ptr)
-  return C.int(candidate.SdpMlineIndex)
+	candidate := (*IceCandidate)(ptr)
+	return C.int(candidate.SdpMlineIndex)
 }
 
 //export go_IceCandidate_GetCandidate
 func go_IceCandidate_GetCandidate(ptr unsafe.Pointer) *C.char {
-  if ptr == nil {
-    return C.CString("")
-  }
+	if ptr == nil {
+		return C.CString("")
+	}
 
-  candidate := (*IceCandidate)(ptr)
-  return C.CString(candidate.Candidate)
+	candidate := (*IceCandidate)(ptr)
+	return C.CString(candidate.Candidate)
 }
--- wrapper.go
+++ wrapper.go
@@ -6,102 +6,102 @@ package webrtc
 import "C"
 
 import (
-  "runtime"
-  "unsafe"
+	"runtime"
+	"unsafe"
 )
 
 func InitializeSSL() {
-  C.WebRTC_InitializeSSL()
+	C.WebRTC_InitializeSSL()
 }
 
 func CleanupSSL() {
-  C.WebRTC_CleanupSSL()
+	C.WebRTC_CleanupSSL()
 }
 
 type (
-  Factory        struct{ ptr C.Factory }
-  MediaStream    struct{ ptr C.MediaStream }
-  PeerConnection struct{ ptr C.PeerConnection }
+	Factory        struct{ ptr C.Factory }
+	MediaStream    struct{ ptr C.MediaStream }
+	PeerConnection struct{ ptr C.PeerConnection }
 )
 
 func New() *Factory {
-  inner := C.WebRTC_PeerConnectionFactory_Create()
-  outer := &Factory{inner}
-  runtime.SetFinalizer(outer, (*Factory).free)
-  return outer
+	inner := C.WebRTC_PeerConnectionFactory_Create()
+	outer := &Factory{inner}
+	runtime.SetFinalizer(outer, (*Factory).free)
+	return outer
 }
 
 func (f *Factory) free() {
-  if f != nil {
-    C.WebRTC_PeerConnectionFactory_Free(f.ptr)
-  }
+	if f != nil {
+		C.WebRTC_PeerConnectionFactory_Free(f.ptr)
+	}
 }
 
 func (f *Factory) CreatePeerConnection(servers []*ICEServer, constraints []*MediaConstraint, observer PeerConnectionObserver) *PeerConnection {
-  var (
-    pservers     unsafe.Pointer
-    pconstraints unsafe.Pointer
-  )
+	var (
+		pservers     unsafe.Pointer
+		pconstraints unsafe.Pointer
+	)
 
-  if len(servers) > 0 {
-    pservers = unsafe.Pointer(&servers[0])
-  }
+	if len(servers) > 0 {
+		pservers = unsafe.Pointer(&servers[0])
+	}
 
-  if len(constraints) > 0 {
-    pconstraints = unsafe.Pointer(&constraints[0])
-  }
+	if len(constraints) > 0 {
+		pconstraints = unsafe.Pointer(&constraints[0])
+	}
 
-  inner := C.WebRTC_PeerConnection_Create(f.ptr,
-    pservers, C.int(len(servers)),
-    pconstraints, C.int(len(constraints)),
-    register(observer))
-  outer := &PeerConnection{inner}
-  runtime.SetFinalizer(outer, (*PeerConnection).free)
-  return outer
+	inner := C.WebRTC_PeerConnection_Create(f.ptr,
+		pservers, C.int(len(servers)),
+		pconstraints, C.int(len(constraints)),
+		register(observer))
+	outer := &PeerConnection{inner}
+	runtime.SetFinalizer(outer, (*PeerConnection).free)
+	return outer
 }
 
 func (s *PeerConnection) free() {
-  if s != nil {
-    C.WebRTC_PeerConnection_Free(s.ptr)
-  }
+	if s != nil {
+		C.WebRTC_PeerConnection_Free(s.ptr)
+	}
 }
 
 func (s *PeerConnection) UpdateIce(servers []*ICEServer, constraints []*MediaConstraint) bool {
-  var (
-    pservers     unsafe.Pointer
-    pconstraints unsafe.Pointer
-  )
+	var (
+		pservers     unsafe.Pointer
+		pconstraints unsafe.Pointer
+	)
 
-  if len(servers) > 0 {
-    pservers = unsafe.Pointer(&servers[0])
-  }
+	if len(servers) > 0 {
+		pservers = unsafe.Pointer(&servers[0])
+	}
 
-  if len(constraints) > 0 {
-    pconstraints = unsafe.Pointer(&constraints[0])
-  }
+	if len(constraints) > 0 {
+		pconstraints = unsafe.Pointer(&constraints[0])
+	}
 
-  if s != nil {
-    ok := C.WebRTC_PeerConnection_UpdateICE(s.ptr,
-      pservers, C.int(len(servers)),
-      pconstraints, C.int(len(constraints)))
+	if s != nil {
+		ok := C.WebRTC_PeerConnection_UpdateICE(s.ptr,
+			pservers, C.int(len(servers)),
+			pconstraints, C.int(len(constraints)))
 
-    if ok == 1 {
-      return true
-    }
-  }
+		if ok == 1 {
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
 func (f *Factory) CreateMediaStream(label string) *MediaStream {
-  inner := C.WebRTC_CreateMediaStreamWithLabel(f.ptr, C.CString(label))
-  outer := &MediaStream{inner}
-  runtime.SetFinalizer(outer, (*MediaStream).free)
-  return outer
+	inner := C.WebRTC_CreateMediaStreamWithLabel(f.ptr, C.CString(label))
+	outer := &MediaStream{inner}
+	runtime.SetFinalizer(outer, (*MediaStream).free)
+	return outer
 }
 
 func (s *MediaStream) free() {
-  if s != nil {
-    C.WebRTC_MediaStream_Free(s.ptr)
-  }
+	if s != nil {
+		C.WebRTC_MediaStream_Free(s.ptr)
+	}
 }
